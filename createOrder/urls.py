05d0010b710937629f816dae84649d387e81b20b
Zac McMillan <84478817+zacmcmillan29@users.noreply.github.com>
2021-12-11 06:54:30 -0700

    fully prepped sir

--- createOrder/urls.py
+++ createOrder/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 # This is accessing the views/funcitons that we wrote on views.py!!
-from .views import editOrderPageView, orderPageView, storeOrderPageView, OrdersSummaryView, updateOrder
+from .views import editOrderPageView, orderPageView, storeOrderPageView, OrdersSummaryView, updateOrder, TicketsSummaryView
 
 
 # we create the path with this syntax
@@ -10,9 +10,11 @@ urlpatterns = [
     path("createorder/", orderPageView, name="orders"),
     path("storeorder/", storeOrderPageView, name='storeorder'),
     path('currentOrders/', OrdersSummaryView, name='currentOrders'),
+    path('currentTickets/', TicketsSummaryView, name='currentTickets'),
     path("editOrder/<int:custID>/",
          editOrderPageView, name='editOrder'),
-    path("updateOrder/", updateOrder, name="updateOrder")
+    path("updateOrder/", updateOrder, name="updateOrder"),
+
     # path("order/", OrdersSummaryView, name="orders"),
     # path("orders/", OrdersSummaryView, name="orders"),
 
