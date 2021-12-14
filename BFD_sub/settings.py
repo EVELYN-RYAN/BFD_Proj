a098de199397690f07e46c3292d2b090991167d9
benjasch97 <schneider.benjamina97@gmail.com>
2021-12-14 09:35:45 -0700

    change database name

--- BFD_sub/settings.py
+++ BFD_sub/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'BFD_sub.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'BFD_DB',
+        'NAME': 'BFD_DB_test',
         'USER': 'postgres',
         'PASSWORD': 'password',
         'HOST': 'localhost',
