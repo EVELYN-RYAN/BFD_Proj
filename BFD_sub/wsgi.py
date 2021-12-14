4f371d2680133c312a194488217cba1678fe5f17
benjasch97 <schneider.benjamina97@gmail.com>
2021-12-14 11:25:29 -0700

    change wsgi.py file

--- BFD_sub/wsgi.py
+++ BFD_sub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'BFD.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bfd_sub.settings')
 
 application = get_wsgi_application()
